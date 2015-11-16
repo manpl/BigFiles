810c72775c891667feb177bb7fa19eaaeb2e3b88
Marcinn <nawrockim@op.pl>
2015-11-16 23:26:42 +0100

    Added skip and take operations

---
+++ src/BigFiles/Operations/SkipOperation.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BigFiles.Operations
+{
+    [Usage("/Skip")]
+    public class SkipOperation : FileOperationBase
+    {
+        public int NoToSkip { get; private set; }
+        public SkipOperation(IOperation parent, string noToSkip) : base(parent) {
+            this.NoToSkip = int.Parse(noToSkip);
+        }
+
+        public override IEnumerable<InputLine> ReadLines()
+        {
+            return this.Parent.ReadLines().Skip(NoToSkip);
+        }
+
+        public override void Execute()
+        {
+            Parent.Execute();
+        }
+    }
+}
---
+++ src/BigFiles/Operations/TakeOperation.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BigFiles.Operations
+{
+    [Usage("/Take")]
+    public class TakeOperation : FileOperationBase
+    {
+        public int NoToTake { get; private set; }
+
+        public TakeOperation(IOperation parent, String noToTake) : base(parent) {
+            this.NoToTake = int.Parse(noToTake);   
+        }
+
+        public override IEnumerable<InputLine> ReadLines()
+        {
+            return this.Parent.ReadLines().Take(NoToTake);
+        }
+
+        public override void Execute()
+        {
+            this.Parent.Execute();
+        }
+    }
+}
